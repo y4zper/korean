3698d47f73a41dc861affe5271982ec852435f45
y4zper <ander.crr@gmail.com>
2025-09-06 12:48:29 -0500

    cambios

--- src/main.jsx
+++ src/main.jsx
@@ -49,7 +49,8 @@ const router = createBrowserRouter([
         path: "/checkout",   // 👈 nueva ruta
         element: <Checkout/>
       }
-    ]
+    ],
+    basename: "/korean"  // 👈 aquí se define el basename
   },
 ]);
 
